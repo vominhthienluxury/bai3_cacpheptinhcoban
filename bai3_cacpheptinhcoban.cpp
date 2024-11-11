4981bb19e13bf51f7e3cdea11ea419c92516b464
vominhthienluxury <thanhsooon1111@gmail.com>
2024-11-11 09:44:13 +0700

    update1

--- bai3_cacpheptinhcoban.cpp
+++ bai3_cacpheptinhcoban.cpp
@@ -1,5 +1,17 @@
 #include <iostream>
 using namespace std;
+double tinhhieu(double a, double b) {
+	return a - b;
+}
+double tinhtong(double a, double b) {
+	return a + b;
+}
+double tinhtich(double a, double b) {
+	return a * b;
+}
+double tinhthuong(double a, double b) {
+	return round (a / b);
+}
 int main() {
 	cout << "CHUONG TRINH TINH CAC PHEP TINH CO BAN\n " << endl;
 	double so1, so2, tong, hieu, tich, thuong;
@@ -11,9 +23,70 @@ int main() {
 	tong = so1 + so2;
 	tich = so1 * so2;
 	thuong = so1 / so2;
-	cout << "Hieu hai so: " << so1 << "-" << so2 << " = " << hieu << endl << endl;
+	cout << "Hieu hai so thong thuong: " << so1 << "-" << so2 << " = " << hieu << endl << endl;
+	cout << "Hieu hai so su dung ham: " << so1 << "-" << so2 << " = " << tinhhieu(so1,so2) << endl << endl;
 	cout << "Tong hai so: " << so1 << "+" << so2 << " = " << tong << endl << endl;
+	cout << "Tong hai so: " << so1 << "+" << so2 << " = " << tinhtong(so1,so2) << endl << endl;
+	cout << "Tich hai so: " << so1 << "*" << so2 << " = " << tinhtich(so1, so2) << endl << endl;
 	cout << "Tich hai so: " << so1 << "*" << so2 << " = " << tich << endl << endl;
+	cout << "THuong hai so: " << so1 << "/" << so2 << " = " << tinhthuong(so1, so2) << endl << endl;
 	cout << "THuong hai so: " << so1 << "/" << so2 << " = " << thuong << endl << endl;
 	return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//onwer: vominhtthienluxury
